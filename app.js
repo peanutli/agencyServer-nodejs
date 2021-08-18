f5537f2d90f2c6f66a43f31a2e0e47157c8bc208
lidachuan <bjlidahcuan@163.com>
2021-08-18 22:49:43 +0800

    feat:更改target地址

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-app.use('^/', createProxyMiddleware({ target: 'http://rap2api.taobao.org/app/mock/287809', changeOrigin: true }));
+// app.use('^/', createProxyMiddleware({ target: 'http://rap2api.taobao.org/app/mock/287809', changeOrigin: true }));
+app.use('^/', createProxyMiddleware({ target: 'https://wifi.12306.cn/operatemonit/wifiapps/ordercenter-query-front-end/api', changeOrigin: true }));
 app.listen(process.env.PORT || 5000)
 
