f6f9804ea5725d5191b8ac6a97780f06bd2d56ee
lidachuan <bjlidahcuan@163.com>
2021-08-18 21:31:26 +0800

    feat:修改代码

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-app.use('/lidachuan', createProxyMiddleware({ target: 'http://rap2api.taobao.org/app/mock/287809', changeOrigin: true }));
+app.use('^/', createProxyMiddleware({ target: 'http://rap2api.taobao.org/app/mock/287809', changeOrigin: true }));
 app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     next()
