e53fc8906d047654af837778927c3c1fc36bc328
lidachuan <bjlidahcuan@163.com>
2021-08-19 06:07:08 +0800

    feat:更新代码

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const express =require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-// app.use('^/', createProxyMiddleware({ target: 'http://rap2api.taobao.org/app/mock/287809', changeOrigin: true }));
-app.use('^/', createProxyMiddleware({ target: 'https://wifi.12306.cn/operatemonit/wifiapps/ordercenter-query-front-end/api', changeOrigin: true }));
-app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    next()
-})
+app.use('^/', createProxyMiddleware({ target: 'http://rap2api.taobao.org/app/mock/287809', changeOrigin: true }));
 app.listen(process.env.PORT || 5000)
 
